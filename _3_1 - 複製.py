07271d7b87d3a76b0cd2aa32437d97c040bdc89e
VICTOR_ACER15\User <audiostore608@gmail.com>
2022-10-24 15:35:05 +0800

    no message

---
+++ _3_1 - 複製.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 10:43:14 2022
+
+@author: User
+"""
+
+data = [[5, 6], [1, 2], [4,8], [7, 9], ]  #(恆a <= b)
+data.sort()#sort
+opTbl = []#123
+opTbl.append(data[0])#45611111
+print(data)
+print(opTbl)
+for i in range(1, len(data), 1):
+    j = len(opTbl) - 1
+    if(data[i][0] <= opTbl[j][0]):#a1 < a0
+        if data[i][1] > opTbl[j][1]:#b1 > b0
+            opTbl[j][0], opTbl[j][1]= data[i][0], data[i][1]
+        else:
+            opTbl[j][0]= data[i][0]
+    if(data[i][1] >= opTbl[j][1]):#b1 > b0
+        if(data[i][0] < opTbl[j][0]):#a1 < a0
+            opTbl[j][0], opTbl[j][1]= data[i][0], data[i][1]
+        else:
+            opTbl[j][1]= data[i][1]
+    else: 
+        print("append",end="")    
+        opTbl.append([data[i][0],data[i][1]])
+    opTbl.sort()
+    print(opTbl)
+
+print(opTbl)
